f98cfb32b223f5ca7caa5480eca9fbc7e73edef8
felipeeleoterio <felipe.eleoterio@aluno.senai.br>
2025-03-17 20:55:32 -0300

    Criando o repositório local  e associando ao repositório online

---
+++ inicio/src/components/FirstComponent.jsx
@@ -0,0 +1,17 @@
+const FirstComponent = () => {
+  return (
+    <div>
+      <h1>Meu incrivel componente</h1>
+      <h2>Tá,nem tão incrivel assim</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        sapiente accusamus minus error id tempora quos quod neque porro, quidem
+        molestiae provident! Magnam aliquam placeat praesentium cupiditate
+        necessitatibus fuga explicabo?</p>
+      <img src="https://images.ctfassets.net/3mv54pzvptwz/7Jj4ryLGJazS8pDUlCK2Vg/07213977dcbbc3d402bac1e1a27c86af/54331642772_05fa9ffe6b_o_dentro.jpg"/>
+
+    </div>
+  )
+}
+
+export default FirstComponent
