f4581876a8f6109b2b7322ffd5af515da428daeb
felipeeleoterio <felipe.eleoterio@aluno.senai.br>
2025-03-21 21:11:32 -0300

    alterando

--- inicio/src/component/ListRender.jsx
+++ inicio/src/component/ListRender.jsx
@@ -2,15 +2,37 @@ import { useState } from "react";
 
 const ListRender = () => {
   const [lista] = useState(["Mateus", "Marcos", "Lucas", "João"]);
-
+  const [users] = useState([
+    {
+      id: 1,
+      name: "Jesus",
+      number: 25,
+    },
+    {
+      id: 7,
+      name: "Cristiano Ronaldo",
+      number: 7,
+    },
+    {
+      id: 10,
+      name: "Neymar Jr",
+      number: 10,
+    },
+  ]);
   return (
     <div>
-      
+      {/*  Lista com index de chave */}
       <ul>
-        {lista.map((item)=> (
-            <li>{item}</li>
+        {lista.map((item, index) => (
+          <li key={index}>{item}</li>
         ))}
       </ul>
+      {/*  Lista com id de chave */}
+         { users.map((usuario) =>( 
+           // console.log(usuario)
+    <h2 key={usuario.id} >O nome é {usuario.name}, e seu número é {usuario.number} </h2>
+    ))}
+    
     </div>
   );
 };
